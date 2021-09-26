5b116b179437e91067edf73e11845a087d6f51a2
Gauri Garg <68108677+gaurigarg33@users.noreply.github.com>
2021-09-26 18:30:54 +0530

    Add files via upload

---
+++ Day11/database/index.js
@@ -0,0 +1,27 @@
+const {Sequelize}= require("sequelize");
+
+const sequelize = new Sequelize(
+    "postgres",
+    "postgres",
+    "123456",
+    {
+        host:"localhost",
+        dialect:"postgres"
+    }
+);
+
+sequelize.sync();
+
+(async()=>
+{
+    try{
+    await sequelize.authenticate();
+    console.log("Connection established with DB");
+    }catch(err)
+    {
+        console.error("Unable to connect to DB");
+    }
+
+})();
+
+module.exports = sequelize;
