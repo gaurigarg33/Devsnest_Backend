bb2d7af9ccea6b5faffe053880597f5ca629c69a
Gauri Garg <68108677+gaurigarg33@users.noreply.github.com>
2021-10-06 23:33:09 +0530

    Update index.js

--- Day9/index.js
+++ Day9/index.js
@@ -1,4 +1,3 @@
-// SENDING STATIC FILES
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -8,12 +7,18 @@ app.set("view engine","jade");
 
 app.use('/' , (req , res)=>
 {
-  // res.sendFile(path.join(__dirname , "public/Hello.txt"),'test.txt')
-   //res.sendFile(path.join(__dirname , "public/text.json"))
-   // res.download(path.join(__dirname , "public/image.jpg"),"download.jpg")
-   //  res.download(path.join(__dirname , "public/text.json"))
+  //res.sendFile(path.join(__dirname , "public/Hello.txt"),'test.txt')
+   //res.sendFile(path.join(__dirname , "public/test.json"))
+  // res.download(path.join(__dirname , "public/test.json"),"hello.json" )
+  //res.download(path.join(__dirname , "public/image.jfif"),"download.jfif")
 
-   res.render('index',{title:"Express"})
+   //res.render('index',{ title:"Express" })
+   res.status(201)
+   .cookie("token","test", {
+       expire:new Date(Date.now() + 8* 3600000)
+   })
+   .cookie("hello","hello")
+   .redirect(301,"/admin")
   
 });
 
